dac2d7e3ef75098b361568fa8c1cc88a18a420a2
Lucas Moschen <lucas.machadomoschen@gmail.com>
2021-10-20 23:42:22 -0300

    last experiments

---
+++ models/extra/sketch-v8.stan
@@ -0,0 +1,117 @@
+functions {
+  /**
+  * Return the log probability of a proper conditional autoregressive (CAR) prior 
+  * with a sparse representation for the adjacency matrix
+  *
+  * @param phi Vector containing the parameters with a CAR prior
+  * @param tau Precision parameter for the CAR prior (real)
+  * @param alpha Dependence (usually spatial) parameter for the CAR prior (real)
+  * @param W_sparse Sparse representation of adjacency matrix (int array)
+  * @param n Length of phi (int)
+  * @param W_n Number of adjacent pairs (int)
+  * @param D_sparse Number of neighbors for each location (vector)
+  * @param lambda Eigenvalues of D^{-1/2}*W*D^{-1/2} (vector)
+  *
+  * @return Log probability density of CAR prior up to additive constant
+  */
+  real sparse_car_lpdf(vector phi, real alpha, 
+    int[,] W_sparse, vector D_sparse, vector lambda, int n, int W_n) {
+      row_vector[n] phit_D; // phi' * D
+      row_vector[n] phit_W; // phi' * W
+      vector[n] ldet_terms;
+    
+      phit_D = (phi .* D_sparse)';
+      phit_W = rep_row_vector(0, n);
+      for (i in 1:W_n) {
+        phit_W[W_sparse[i, 1]] = phit_W[W_sparse[i, 1]] + phi[W_sparse[i, 2]];
+        phit_W[W_sparse[i, 2]] = phit_W[W_sparse[i, 2]] + phi[W_sparse[i, 1]];
+      }
+    
+      for (i in 1:n) ldet_terms[i] = log1m(alpha * lambda[i]);
+      return 0.5 * (sum(ldet_terms) - (phit_D * phi - alpha * (phit_W * phi)));
+  }
+  real gumbel_type2_lpdf(real tau, real lambda){
+    return -(3.0/2.0 * log(tau) + lambda / sqrt(tau)); 
+  }
+  real inv_gumbel_type2_lpdf(real sigma, real lambda){
+    return -lambda * sigma;
+  }
+}
+data {
+  int<lower = 1> n;
+  int<lower = 1> p;
+  int<lower = 0, upper = 1> y[n];
+  matrix[n,p] X;
+
+  matrix<lower = 0, upper = 1>[n, n] W; // adjacency matrix
+  int W_n;                // number of adjacent region pairs
+
+  real<lower = 0> alpha_tau;
+  real<lower = 0> beta_tau;
+  real<lower = 0> lambda_tau; 
+  real<lower = 0> M_tau;
+
+  real<lower = 0> alpha_prev;
+  real<lower = 0> beta_prev;
+
+  vector[p] mu_beta;
+  cov_matrix[p] cov_beta;
+  
+  int<lower = 0, upper = 1> gumbel_prior;
+}
+transformed data {
+  matrix[p,p] sigma_beta; 
+  int W_sparse[W_n, 2];   // adjacency pairs
+  vector[n] D_sparse;     // diagonal of D (number of neigbors for each site)
+  vector[n] lambda;       // eigenvalues of invsqrtD * W * invsqrtD
+
+  sigma_beta = cholesky_decompose(cov_beta);
+  
+  { // generate sparse representation for W
+  int counter;
+  counter = 1;
+  // loop over upper triangular part of W to identify neighbor pairs
+    for (i in 1:(n - 1)) {
+      for (j in (i + 1):n) {
+        if (W[i, j] == 1) {
+          W_sparse[counter, 1] = i;
+          W_sparse[counter, 2] = j;
+          counter = counter + 1;
+        }
+      }
+    }
+  }
+  for (i in 1:n) D_sparse[i] = sum(W[i]);
+  {
+    vector[n] invsqrtD;  
+    for (i in 1:n) {
+      invsqrtD[i] = 1 / sqrt(D_sparse[i]);
+    }
+    lambda = eigenvalues_sym(quad_form(W, diag_matrix(invsqrtD)));
+  }
+}
+parameters {
+  vector[n] phi;
+  real<lower = 0, upper = M_tau> tau;
+  real<lower = 0, upper = 1> prev;
+  vector[p] normal_raw; 
+  real<lower = 0, upper = 0.9> alpha;
+}
+transformed parameters {
+   vector[p] beta; 
+   beta = mu_beta + sigma_beta * normal_raw; 
+}
+model {
+  if (gumbel_prior == 1) {
+     tau ~ gumbel_type2(lambda_tau);
+  } else {
+     //tau ~ gamma(alpha_tau, beta_tau);
+     tau ~ uniform(0, M_tau);
+  }
+    
+  phi ~ sparse_car(alpha, W_sparse, D_sparse, lambda, n, W_n);
+  normal_raw ~ std_normal();   
+  prev ~ beta(alpha_prev, beta_prev);
+  
+  y ~ bernoulli_logit(logit(prev) + X * beta + (1/sqrt(tau)) * phi);
+}
--- scripts/utilits.py
+++ scripts/utilits.py
@@ -8,41 +8,38 @@ class ParameterAlpha:
     def __init__(self) -> None:
         pass
 
-    def relative_quadratic_error(self,x,y): 
+    def relative_quadratic_error(self,x,y):
         return (x - y)**2/x**2
 
-    def quadratic_error(self,x,y): 
+    def quadratic_error(self,x,y):
         return (x - y)**2
 
-    def loss_function(self, alpha, m1, m2, v1, v2, rho, g, c = [1,1,1,1]): 
+    def loss_function(self, alpha, m1, m2, v1, v2, rho, g, c=(1, 1, 1, 1)):
         """
         This function calculates the loss function
-        g(m1,E[X1])+g(m2,E[X2])+g(v1,Var[X1])+g(v2,Var[X2])+g(rho,Cor(X1,X2)) 
+        g(m1,E[X1])+g(m2,E[X2])+g(v1,Var[X1])+g(v2,Var[X2])+g(rho,Cor(X1,X2))
         as function of alpha, when the means (m1,m2) and variances (v1,v2)
         from sensitivity and specificity are fixed, and the correlation rho
-        between them. 
+        between them.
         - alpha: vector[4]
-        - m1,m2,v1,v2,rho: float between 0 and 1. 
+        - m1,m2,v1,v2,rho: float between 0 and 1.
         - g: a function that receives to real values and return a real value.
         - c: vector[4] represents the weights. Default: [1,1,1,1].
         """
         alpha_tilde = sum(alpha)
-        div = alpha_tilde*alpha_tilde*(alpha_tilde + 1)
-        
+        div = alpha_tilde*alpha_tilde*(alpha_tilde + 1)     
         alpha_12 = alpha[0] + alpha[1]
         alpha_34 = alpha[2] + alpha[3]
         alpha_13 = alpha[0] + alpha[2]
         alpha_24 = alpha[1] + alpha[3]
-        
-        obj  = c[0]*g(m1, alpha_12/alpha_tilde)
+        obj = c[0]*g(m1, alpha_12/alpha_tilde)
         obj += c[1]*g(m2, alpha_13/alpha_tilde)
         obj += c[2]*g(v1, alpha_12*alpha_34/div)
         obj += c[3]*g(v2, alpha_13*alpha_24/div)
         obj += g(rho, (alpha[0]*alpha[3] - alpha[1]*alpha[2])/(np.sqrt(alpha_12*alpha_34*alpha_13*alpha_24)))
-        
         return obj
 
-    def partial_loss_function(self, alpha34, m1, m2, v1, v2, rho, g, c): 
+    def partial_loss_function(self, alpha34, m1, m2, v1, v2, rho, g, c):
 
         alpha3, alpha4 = alpha34[0], alpha34[1]
         alpha1 = ((m1 + m2 - 1) * alpha3 + m2 * alpha4) / (1 - m1)
@@ -50,33 +47,36 @@ class ParameterAlpha:
         alpha_tilde = alpha1 + alpha2 + alpha3 + alpha4
 
         rho_tilde = (alpha1 * alpha4 - alpha2 * alpha3) / (alpha_tilde * alpha_tilde)
-        rho_tilde /= np.sqrt(m1 * m2 * (1-m1) * (1-m2)) 
-        
+        rho_tilde /= np.sqrt(m1 * m2 * (1-m1) * (1-m2))
         obj = 0
         obj += c[0]*g(m1 * (1 - m1) / v1, alpha_tilde)
         obj += c[1]*g(m2 * (1 - m2) / v2, alpha_tilde)
         obj += g(rho, rho_tilde)
-        
         return obj
 
 
-    def minimizer(self, m1, m2, v1, v2, rho, 
-                  c = [1,1,1,1], 
-                  g = 'quadratic', 
-                  x0 = (1,1,1,1)): 
+    def minimizer(self, m1, m2, v1, v2, rho,
+                  c=(1, 1, 1, 1),
+                  g='quadratic',
+                  x0=(1, 1, 1, 1)):
         """
-        Minimized the loss function as function of alpha. 
+        Minimized the loss function as function of alpha.
         """
-        if g == 'relative_quadratic': 
+        if g == 'relative_quadratic':
             g = self.relative_quadratic_error
         else: 
             g = self.quadratic_error      
 
-        return minimize(fun = self.loss_function, 
-                        x0 = x0, 
-                        args = (m1, m2, v1, v2, rho, g, c),
-                        bounds = [(0, np.inf)]*4,
-                        method = 'trust-constr')
+        return minimize(fun=self.loss_function, 
+                        x0=x0, 
+                        args=(m1, m2, v1, v2, rho, g, c),
+                        bounds=[(0, np.inf)]*4,
+                        constraints={'type': 'ineq', 
+                                     'fun': lambda alpha, m1, m2, v1, v2: max(m1*(1-m1)/v1 - 1,
+                                                                              m2*(1-m2)/v2-1) \
+                                                                          - sum(alpha),
+                                     'args': [m1, m2, v1, v2]},
+                        method='trust-constr')
 
     def solve_equations(self, m1, m2, v1, v2, rho):
         """
@@ -88,35 +88,33 @@ class ParameterAlpha:
         rho_expression = rho + np.sqrt((1-m1)*(1-m2)/(m1*m2))
         rho_expression *= np.sqrt(m1*m2*(1-m1)*(1-m2))*(m1 - m1*m1 - v1)
         alpha4 = rho_expression/v1
-
-        if alpha4 < 0: 
+        if alpha4 < 0:
             raise Exception('This system does not have a solution since alpha_4 < 0')
-     
         # finds alpha1,alpha2,alpha3 as function of alpha4, m1, m2, and v1
-        C = (m1 - m1*m1 - v1)/v1 
-        alpha1 = (m1 + m2 - 1)*C + alpha4 
-        alpha2 = (1 - m2)*C - alpha4 
-        alpha3 = (1 - m1)*C - alpha4 
+        C = (m1 - m1*m1 - v1)/v1
+        alpha1 = (m1 + m2 - 1)*C + alpha4
+        alpha2 = (1 - m2)*C - alpha4
+        alpha3 = (1 - m1)*C - alpha4
 
         if alpha1 < 0: 
             raise Exception('This system does not have a solution since alpha_1 < 0')
-        elif alpha2 < 0: 
+        elif alpha2 < 0:
             raise Exception('This system does not have a solution since alpha_2 < 0')
-        elif alpha3 < 0: 
+        elif alpha3 < 0:
             raise Exception('This system does not have a solution since alpha_3 < 0')
-     
-        # With the estimated alpha, the value of v2_hat. 
+
+        # With the estimated alpha, the value of v2_hat.
         v2_hat = v1*(1-m2)/(m1*(1-m1))
 
         err_v2 = abs(v2 - v1*(1-m2)/(m1*(1 - m1)))/v2
 
         return ((alpha1, alpha2, alpha3, alpha4), v2_hat, err_v2)
 
-    def mix_solver(self, m1, m2, v1, v2, rho, 
-                  c = [1,1], 
-                  g = 'quadratic', 
-                  x0 = (1,1), 
-                  lb = 0): 
+    def mix_solver(self, m1, m2, v1, v2, rho,
+                   c=(1, 1),
+                   g='quadratic',
+                   x0=(1, 1),
+                   lb=0):
         """
         It only fixes the means while the others are minimized.
         """
@@ -125,11 +123,11 @@ class ParameterAlpha:
         else: 
             g = self.quadratic_error    
 
-        minimized = minimize(fun = self.partial_loss_function, 
-                             x0 = x0, 
-                             args = (m1, m2, v1, v2, rho, g, c),
-                             bounds = [(lb, np.inf)]*2,
-                             method = 'trust-constr')
+        minimized = minimize(fun=self.partial_loss_function, 
+                             x0=x0, 
+                             args=(m1, m2, v1, v2, rho, g, c),
+                             bounds=[(lb, np.inf)]*2,
+                             method='trust-constr')
 
         alpha3, alpha4 = minimized.x
 
@@ -140,7 +138,6 @@ class ParameterAlpha:
             raise Exception('This system does not have a solution since alpha_1 < 0')
         if alpha2 < 0:
             raise Exception('This system does not have a solution since alpha_1 < 0')
-            
         return (alpha1, alpha2, alpha3, alpha4)
 
 class BivariateBeta:
@@ -149,20 +146,13 @@ class BivariateBeta:
         pass
 
     def moments_calculus(self, alpha): 
-        
         tilde_alpha = alpha[0]+alpha[1]+alpha[2]+alpha[3]
-        
         E_X = (alpha[0]+alpha[1])/tilde_alpha
         E_Y = (alpha[0]+alpha[2])/tilde_alpha
-        
         Var_X = (1/(tilde_alpha*(tilde_alpha+1)))*E_X*(alpha[2] + alpha[3])
         Var_Y = (1/(tilde_alpha*(tilde_alpha+1)))*E_Y*(alpha[1] + alpha[3])
-        
-        den = np.log(alpha[0] + alpha[1]) + np.log(alpha[2]+alpha[3]) + np.log(alpha[0]+alpha[2]) + np.log(alpha[1]+alpha[3])
+        den = np.log(alpha[0] + alpha[1]) + np.log(alpha[2]+alpha[3]) 
+        den += np.log(alpha[0]+alpha[2]) + np.log(alpha[1]+alpha[3])
         den = np.exp(-0.5*den)
         Cor_XY = (alpha[0]*alpha[3] - alpha[1]*alpha[2])*den
-        
         return (E_X, E_Y, Var_X, Var_Y, Cor_XY)
-
-
-    
