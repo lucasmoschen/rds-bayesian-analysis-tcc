d910c032aabed52a8a8b506ce67a6fde385c25c0
Lucas Moschen <lucas.machadomoschen@gmail.com>
2021-11-22 08:06:37 -0300

    ideia !

--- scripts/crawford_algorithm.py
+++ scripts/crawford_algorithm.py
@@ -2,6 +2,7 @@
 """
 Crawford Metropolis-within-Gibbs algorithm
 """
+from networkx.classes.function import neighbors
 import numpy as np
 import networkx as nx
 
@@ -29,11 +30,27 @@ class Crawford:
 
         degree_prob = np.array(list(degrees.values()))
         degree_prob = 1.0 * degree_prob / degree_prob.sum()
-        seeds = ro.choice(list(graph.nodes), p=degree_prob, size=n_seeds)
+        seeds = ro.choice(list(graph.nodes), p=degree_prob, size=n_seeds,
+                          replace=False)
 
-        
+        recruited = {seeds[i]: [0, coupons[i]] for i in range(n_seeds)}
+        susceptible_edges = {s: {neighbor: graph.edges[(s, neighbor)]
+                                    for neighbor in graph.neighbors(s)}
+                             for s in seeds}
+        timer = 0
+        for new_node in range(n_seeds, n_samples):
+            new_time = np.inf
+            for rec, info in recruited.items():
+                if info[1] > 0:
+                    for s, time in susceptible_edges[rec].items():
+                        ev_time = info[0] + time['edge_time']
+                        if ev_time < new_time:
+                            new_time = ev_time
+                            new_participant = s
+            recruited[new_participant] = [new_time, coupons[new_node]]
+                        
 
 if __name__ == '__main__':
     simulation = Crawford()
-    graph = nx.gnp_random_graph(n=1000, p=0.05)
-    simulation.simulating_rds_crawford(graph, 10, 10, 1, [1/3, 1/6, 1/6, 1/3], 371209)
+    graph = nx.gnp_random_graph(n=100, p=0.05)
+    simulation.simulating_rds_crawford(graph, 10, 1, 1, [1/3, 1/6, 1/6, 1/3], 371209)
